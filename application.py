f0b3b0b8379620e30a19cb8bc9f10f0fd18fb4c7
Grant Seastream <grant.seastream@gmail.com>
2021-06-20 15:18:59 -0400

    Remove signup testing

--- application.py
+++ application.py
@@ -39,8 +39,7 @@ def signup():
     form = SignupForm()
     if form.validate_on_submit():
         user = User(username=form.username.data.lower(),
-                    password=form.password.data,
-                    email="grant.seastream+testing@gmail.com")
+                    password=form.password.data)
         db.session.add(user)
 
         db.session.flush()
